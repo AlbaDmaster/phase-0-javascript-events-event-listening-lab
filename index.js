8da87acd6c6b9f3e1e398ca1001f7bd77eeccab6
Al-bashir <albashirabdi482@gmail.com>
2024-04-21 19:33:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+
+
+
+
 function addingEventListener() {
-}
+    let Event=document.getElementById('button');
+
+Event.addEventListener('click',()=> alert('Goodbye'));
+
+return Event;
+    }
+
+    addingEventListener();
